7ccfb81e79c8adabffc186554bca15327f76727e
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2024-01-30 13:04:18 +0200

    added some procedures to help the admin edit some information in the database, fixes #4

---
+++ Database/iStudy-procedures.sql
@@ -0,0 +1,42 @@
+/* Some procedures to help the admin to modify information about students, lecturers, and courses */
+
+CREATE OR REPLACE PROCEDURE modify_student_dept(v_student_id NUMBER, v_dept_id NUMBER)
+IS
+BEGIN
+    UPDATE Students
+    SET DepartmentID = v_dept_id
+    WHERE StudentID = v_student_id;
+END;
+
+CREATE OR REPLACE PROCEDURE update_lecturer_salary(v_lecturer_id NUMBER, v_salary NUMBER)
+IS
+BEGIN
+    UPDATE Lecturers
+    SET Salary = v_salary
+    WHERE LecturerID = v_lecturer_id;
+END;
+
+CREATE OR REPLACE PROCEDURE move_lecturer(v_lecturer_id NUMBER, v_office_room VARCHAR2, v_dept_id NUMBER)
+IS
+BEGIN
+    UPDATE Lecturers
+    SET DepartmentID = v_dept_id,
+        OfficeRoom = v_office_room
+    WHERE LecturerID = v_lecturer_id;
+END;
+
+CREATE OR REPLACE PROCEDURE change_credit_hours(v_course_id NUMBER, v_credit_hours NUMBER)
+IS
+BEGIN
+    UPDATE Courses
+    SET CreditHours = v_credit_hours
+    WHERE CourseID = v_course_id;
+END;
+
+CREATE OR REPLACE PROCEDURE change_course_lecturer(v_course_id NUMBER, v_lecturer_id NUMBER)
+IS
+BEGIN
+    UPDATE Courses
+    SET LecturerID = v_lecturer_id
+    WHERE CourseID = v_course_id;
+END;
