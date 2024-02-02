542ef23437a384d62ce5dad0f17812dab3e93743
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2024-02-03 01:04:07 +0200

    added the logic of the Update Student button, fixes #3

--- Java Application/Admin/Controller/StudentsController.java
+++ Java Application/Admin/Controller/StudentsController.java
@@ -7,6 +7,7 @@ package Controller;
 
 import DAO.DBConnection;
 import DTO.StudentDept;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -18,15 +19,29 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 
 /**
@@ -38,6 +53,8 @@ public class StudentsController implements Initializable {
 
     static ObservableList<StudentDept> students;
     private static boolean studentsLoaded = false;
+    private static boolean selected = false;
+    StudentDept selectedStudent;
     
     @FXML
     private TableView<StudentDept> studentsTable;
@@ -51,6 +68,18 @@ public class StudentsController implements Initializable {
     private TableColumn<StudentDept, Double> studentCGPa;
     @FXML
     private TableColumn<StudentDept, Integer> studentCHours;
+    @FXML
+    private Button addStudentButton;
+    @FXML
+    private ImageView addImage;
+    @FXML
+    private Button deleteStudentButton;
+    @FXML
+    private ImageView deleteImage;
+    @FXML
+    private Button updateStudentButton;
+    @FXML
+    private ImageView updateImage;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -83,8 +112,6 @@ public class StudentsController implements Initializable {
                             // setting the width and height for the image
                             imageView.setFitWidth(width);
                             imageView.setFitHeight(height);
-                            //Circle defaultClip = createCircularClip();
-                            //imageView.setClip(defaultClip);
                             imageView.setClip(createCircularImageView(radius));
 
                             setGraphic(imageView);
@@ -124,6 +151,57 @@ public class StudentsController implements Initializable {
                 Logger.getLogger(StudentsController.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+        
+        // Make the table's rows clickable to be able to visit the friend's profile
+        studentsTable.setRowFactory(new Callback<TableView<StudentDept>, TableRow<StudentDept>>() {
+            @Override
+            public TableRow<StudentDept> call(TableView<StudentDept> tv) {
+                TableRow<StudentDept> userRow = new TableRow<>();
+                userRow.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getClickCount() == 1 && (!userRow.isEmpty())) {
+                            selected = true;
+                            selectedStudent = userRow.getItem();
+                            UpdateStudentController updateStudent = new UpdateStudentController();
+                            updateStudent.processStudentDept(selectedStudent);
+                        }
+                    }
+                });                
+                
+                return userRow ;
+            }
+        });
+        
+        updateStudentButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    if(selected){
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/UpdateStudent.fxml"));
+                        AnchorPane updateStudentScene = loader.load();
+                        Stage blockingWindow = new Stage();
+                        blockingWindow.initModality(Modality.APPLICATION_MODAL);
+                        blockingWindow.getIcons().add(new Image("/resources/logo.png"));
+                        blockingWindow.setTitle("Update Student");
+                        blockingWindow.setScene(new Scene(updateStudentScene));
+                        blockingWindow.showAndWait();
+                        // Refresh the students list
+                        getStudents();
+                        studentsTable.setItems(students);
+                    } else {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setContentText("Please click on a student to change his department.");
+                        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+                        stage.setTitle("Update Student");
+                        stage.getIcons().add(new Image(this.getClass().getResource("/resources/logo.png").toString()));
+                        alert.showAndWait();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } 
+        });
     }    
     
     public static void getStudents() throws SQLException {
@@ -135,11 +213,12 @@ public class StudentsController implements Initializable {
             resultSet.previous();
             while (resultSet.next()) {
                 StudentDept studentDept = new StudentDept(
+                    resultSet.getInt(1),
                     resultSet.getString(2),
-                    resultSet.getString(1),
-                    resultSet.getDouble(4),
-                    resultSet.getInt(5),
-                    resultSet.getString(3)
+                    resultSet.getString(3),
+                    resultSet.getString(4),
+                    resultSet.getDouble(5),
+                    resultSet.getInt(6)
                 );
                 studentsList.add(studentDept);
             }
