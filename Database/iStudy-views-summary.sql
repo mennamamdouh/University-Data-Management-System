8798e6a8b96e9f5eca76737f1853f3c594f0af74
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2024-02-11 15:01:40 +0200

    edited some of the summary views, fixes #4

--- Database/iStudy-views-summary.sql
+++ Database/iStudy-views-summary.sql
@@ -1,25 +1,32 @@
 -- View to show the number of courses related to each department
 CREATE OR REPLACE VIEW courses_in_departments AS
-    SELECT D.DepartmentName, COUNT(C.CourseID) AS numberOfCourses
+    SELECT D.DepartmentName, COUNT(C.CourseID) AS number_of_courses
     FROM Departments D
     INNER JOIN Courses C
         ON D.DepartmentID = C.DepartmentID
-    GROUP BY C.DepartmentID, D.DepartmentName;
+    GROUP BY D.DepartmentName
+    ORDER BY COUNT(C.CourseID) DESC;
 
--- View to show the number of lecturers work in each department
-CREATE OR REPLACE VIEW lecturers_in_departments AS
-    SELECT D.DepartmentName, COUNT(L.LecturerID) AS numberOfLecturers
-    FROM Departments D
-    INNER JOIN Lecturers L
-        ON D.DepartmentID = L.DepartmentID
-    GROUP BY L.DepartmentID, D.DepartmentName;
+-- View to show number of students registered in each semester
+CREATE OR REPLACE VIEW students_per_semester AS
+    SELECT E.Term, COUNT(S.StudentID) AS number_of_students
+    FROM Enrollments E
+    INNER JOIN Students S
+        ON E.StudentID = S.StudentID
+    GROUP BY E.Term
+    ORDER BY COUNT(S.StudentID) DESC;
 
--- View to show number of students enrolled on a course in each semester for each year
-CREATE OR REPLACE VIEW courses_per_semester AS
-    SELECT Term, Year, COUNT(StudentID) AS numberOfStudents
-    FROM Enrollments
-    GROUP BY Term, Year;
+-- View to show the averga GPA for each course
+CREATE OR REPLACE VIEW average_gpa_per_course AS
+    SELECT C.Title, AVG(G.GPA) AS average_gpa
+    FROM Enrollments E
+    INNER JOIN Grades G
+        ON E.Grade = G.Grade
+    INNER JOIN Courses C
+        ON E.CourseID = C.CourseID
+    GROUP BY C.Title;
 
+-- View to show the enrolled students of a specific course
 CREATE OR REPLACE VIEW enrolled_students AS
     SELECT DISTINCT S.StudentID, S.PersonalPhoto, S.FullName, C.CourseID
     FROM Students S
